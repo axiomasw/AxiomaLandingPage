4c5d95784b9500085b04e21e4bb3f5bde9269aa3
Luciana Maldonado <110115736+Luu-maldonado@users.noreply.github.com>
2025-12-11 20:14:50 -0300

    google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,8 @@ import { Geist, Geist_Mono } from "next/font/google"
 import { Analytics } from "@vercel/analytics/next"
 import "./globals.css"
 
+import Script from "next/script"
+
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
@@ -150,8 +152,23 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/images/axima ico.png" />
         <link rel="shortcut icon" href="/images/axima ico.png" />
         <link rel="apple-touch-icon" href="/images/axima ico.png" />
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MM3RXH22');`}
+        </Script>
+        {/* End Google Tag Manager */}
       </head>
       <body className={`font-sans antialiased`}>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MM3RXH22"
+            height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
         {children}
         <Analytics />
       </body>
